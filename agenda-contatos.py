935d05c23cde78705de8bd0db901480b85a679e0
Luigi Tormim <luiginhotoca@gmail.com>
2026-09-09 11:45:31 -0300

    Menu de opcoes criado

---
+++ agenda-contatos.py
@@ -0,0 +1,11 @@
+contatos = []
+while True:
+  print("1. Adicionar contato")
+  print("2. Ver contatos")
+  print("3 - Editar contato")
+  print("4. Favoritar")
+  print("5. Ver favoritos")
+  print("6. Apagar contato")
+  print("7. Sair")
+
+  opcao = input("Digite um numero para executar alguma acao:")
